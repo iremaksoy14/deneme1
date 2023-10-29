3440ceb8ead2ae98ed87ade36346058085645ff7
İrem Aksoy <aksoyirem612@gmail.com>
2023-10-29 16:24:43 +0300

    ..

--- src/app/Routes.js
+++ src/app/Routes.js
@@ -33,28 +33,11 @@ export function Routes() {
 
   return (
     <Switch>
-      {!isAuthorized ? (
-        /*Render auth page when user at `/auth` and not authorized.*/
-        <Route>
-          <AuthPage />
-        </Route>
-      ) : 
-        /*Otherwise redirect to root page (`/`)*/
-        <Redirect from="/auth" to="/" />
-      }
-
-      <Route path="/error" component={ErrorsPage} />
-      <Route path="/logout" component={Logout} />
-
-      {!isAuthorized ? null
-          : (
-        /*Redirect to `/auth` when user is not authorized*/
-        
-     
+    
         <Layout>
           <BasePage />
         </Layout>
-      )}
+     
     </Switch>
   );
 }
